4f92efa29b39c2905bc91fee3a49a32ac780a5c2
INFANTJOY <infantjoy123@gmail.com>
2024-02-22 10:13:12 +0530

    buildupdate

--- src/SquareComponent.jsx
+++ src/SquareComponent.jsx
@@ -1,3 +1,14 @@
-export default function Square(props){
-    return <div className="square">{props.title}</div>;
+import PropTypes from "prop-types";
+
+export default function Square({onClick, isFree, title}){
+    
+    return <div className="square" onClick={onClick}>
+        <span className="banner">{isFree ? "Free":"Paid"}</span>
+        {title}
+        </div>;
 }
+Square.propTypes = {
+    title: PropTypes.string,
+    isFree: PropTypes.bool,
+    onClick: PropTypes.func
+};
